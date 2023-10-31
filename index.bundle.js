41928800f9bc7abbb0534ec93ad6f5e52d03726a
Gasay-Liza <gasayliz@yandex.ru>
2023-10-31 23:02:04 +0300

    Updates

---
+++ index.bundle.js
@@ -0,0 +1,79 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/pages/index/index.js":
+/*!**********************************!*\
+  !*** ./src/pages/index/index.js ***!
+  \**********************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.html */ \"./src/pages/index/index.html\");\n\n\n//# sourceURL=webpack://a-home/./src/pages/index/index.js?");
+
+/***/ }),
+
+/***/ "./src/pages/index/index.html":
+/*!************************************!*\
+  !*** ./src/pages/index/index.html ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// Module\nvar code = \"<!DOCTYPE html>\\r\\n<html lang=\\\"ru\\\">\\r\\n<head>\\r\\n  <meta charset=\\\"UTF-8\\\">\\r\\n  <meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\">\\r\\n  <title>Document</title>\\r\\n</head>\\r\\n<body>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./main.html\\\">Главная страница</a>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./projects.html\\\">Наши проекты</a>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./detal-project.html\\\">Детальная проекта</a>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./services.html\\\">Услуги</a>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./detal-services.html\\\">Детальная услуги</a>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./about.html\\\">О компании</a>\\r\\n  <a class=\\\"title-h3\\\" href=\\\"./404.html\\\">404</a>\\r\\n</body>\\r\\n</html>\";\n// Exports\n/* harmony default export */ __webpack_exports__[\"default\"] = (code);\n\n//# sourceURL=webpack://a-home/./src/pages/index/index.html?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	!function() {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = function(exports) {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/pages/index/index.js");
+/******/ 	
+/******/ })()
+;
