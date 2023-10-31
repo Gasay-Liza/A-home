045cdd7bd1035a179f4631a71a2e809381d8dfeb
Gasay-Liza <gasayliz@yandex.ru>
2023-10-31 16:50:16 +0300

    feat: services page desk

--- src/pages/services/services.js
+++ src/pages/services/services.js
@@ -1,3 +1,26 @@
 import "../../utils/fonts/fonts.css";
 import "../../styles/main.scss";
 import "./services.html";
+import $ from "jquery";
+
+$(document).ready(function () {
+    $(".switcher__btn").on("click", function (evt) {
+      evt.preventDefault();
+      let $this = $(this);
+      let index = $this.index();
+      let $switcher = $this.closest(".switcher");
+      let $switcher_container = $switcher.find(".switcher__container");
+      let $switcher_contents = $switcher_container.find(".switcher__content");
+      $this.siblings(".switcher__btn").removeClass("active");
+      $this.addClass("active");
+      $switcher_contents.removeClass("active");
+      $($switcher_contents[index]).addClass("active");
+    });
+});
+
+$(document).ready(function() {
+  $('.accordion-header').click(function() {
+    $(this).toggleClass('active');
+    $(this).next('.accordion-content').slideToggle();
+  });
+});
